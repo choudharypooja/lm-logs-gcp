e9312f4bfb1f2b69080ee06e2e35f1ca657b9d9b
Pooja Choudhary <79826970+choudharypooja@users.noreply.github.com>
2022-07-12 16:43:28 +0530

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y rubygems build-essential &&
 sudo apt-get install -y ruby-dev &&
 sudo gem install fluentd --no-doc &&
 sudo gem install fluent-plugin-lm-logs &&
-sudo gem install fluent-plugin-gcloud-pubsub-custom && 
+sudo gem install fluent-plugin-gcloud-pubsub-custom=1.4.0 && 
 curl  https://raw.githubusercontent.com/choudharypooja/lm-logs-gcp/main/fluentd.conf --output template.conf &&
 envsubst < template.conf  | cat > fluentd.conf
 fluentd -c ./fluentd.conf
